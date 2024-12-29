9acfebde36b164ac84f1f5b1e3c2bc5fddababc0
ronyderra <derrarony@gmail.com>
2024-12-29 16:39:14 +0200

    asdfad

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,69 +1,69 @@
 // Review Slider
 jQuery(document).ready(function () {
-    const slider = jQuery('.review-slider');
-    if (jQuery.fn.slick && slider.length) {
-        slider.slick({
-            dots: true,
-            infinite: false,
-            arrows: false,
-            speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            responsive: [
-                { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 3, infinite: true }},
-                { breakpoint: 990, settings: { slidesToShow: 2, slidesToScroll: 2 }},
-                { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 }},
-            ]
-        });
-    }
-    $('.toggle-btn').on('click', function (e) {
-        e.preventDefault();
-      
-        // Find the lender-item-more and lender-card elements
-        var $lenderItemMore = $(this).closest('.lender-info').find('.lender-item-more');
-        var $lenderCard = $(this).closest('.lender-card');
-      
-        // Toggle visibility and button text with animation
-        $lenderItemMore.stop(true, true).slideToggle('slow', function() {
-          // Calculate the reduced width by subtracting 20%
-          var cardWidth = $lenderCard.outerWidth() * 0.78; 
-      
-          $(this).css('width', cardWidth + 'px'); // Adjust width after animation
-      
-          if ($(this).is(':visible')) {
-            $(this).prevAll('.toggle-btn').hide(); 
-          } else {
-            $(this).prevAll('.toggle-btn').show(); 
-          }
-        });
-      });
-      
-      // Adjust the width dynamically on window resize
-      $(window).on('resize', function () {
-        $('.lender-item-more:visible').each(function () {
-          var $lenderCard = $(this).closest('.lender-card');
-          var cardWidth = $lenderCard.outerWidth() * 0.78; 
-          $(this).css('width', cardWidth + 'px'); // Adjust width on resize
-        });
-      });
-    
-
-      $('.menu-toggle').on('click', function () {
-        $('body').toggleClass('menu-open'); // Toggle class on the body
-        $(this).toggleClass('active'); // Toggle class on the clicked button
+  const slider = jQuery('.review-slider');
+  if (jQuery.fn.slick && slider.length) {
+    slider.slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 3, infinite: true } },
+        { breakpoint: 990, settings: { slidesToShow: 2, slidesToScroll: 2 } },
+        { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+      ]
     });
+  }
+  $('.toggle-btn').on('click', function (e) {
+    e.preventDefault();
 
-// Function to show the modal
-function showModal() {
-  $(".confused-modal").fadeIn(500).css("display", "flex"); // Show modal with flex display
-  $("body").addClass("popup-active"); // Add class to body for styling adjustments
+    // Find the lender-item-more and lender-card elements
+    var $lenderItemMore = $(this).closest('.lender-info').find('.lender-item-more');
+    var $lenderCard = $(this).closest('.lender-card');
 
-  // Close modal on close button click
-  $(".confused-modal .close-button").on("click", function () {
+    // Toggle visibility and button text with animation
+    $lenderItemMore.stop(true, true).slideToggle('slow', function () {
+      // Calculate the reduced width by subtracting 20%
+      var cardWidth = $lenderCard.outerWidth() * 0.78;
+
+      $(this).css('width', cardWidth + 'px'); // Adjust width after animation
+
+      if ($(this).is(':visible')) {
+        $(this).prevAll('.toggle-btn').hide();
+      } else {
+        $(this).prevAll('.toggle-btn').show();
+      }
+    });
+  });
+
+  // Adjust the width dynamically on window resize
+  $(window).on('resize', function () {
+    $('.lender-item-more:visible').each(function () {
+      var $lenderCard = $(this).closest('.lender-card');
+      var cardWidth = $lenderCard.outerWidth() * 0.78;
+      $(this).css('width', cardWidth + 'px'); // Adjust width on resize
+    });
+  });
+
+
+  $('.menu-toggle').on('click', function () {
+    $('body').toggleClass('menu-open'); // Toggle class on the body
+    $(this).toggleClass('active'); // Toggle class on the clicked button
+  });
+
+  // Function to show the modal
+  function showModal() {
+    $(".confused-modal").fadeIn(500).css("display", "flex"); // Show modal with flex display
+    $("body").addClass("popup-active"); // Add class to body for styling adjustments
+
+    // Close modal on close button click
+    $(".confused-modal .close-button").on("click", function () {
       $(".confused-modal").fadeOut(500); // Fade out modal
       $("body").removeClass("popup-active"); // Remove class from body
-  });
-}
+    });
+  }
 
   // Hide the modal initially
   $(".confused-modal").hide();
@@ -82,22 +82,69 @@ function showModal() {
 
     // Check if the current page is in the list of target pages
     if (targetPages.includes(currentPage)) {
-        // Check if the modal has already been shown this session
-        if (event.clientY <= 0 && !sessionStorage.getItem("modalShown")) {
-            showModal();
-            // Set a flag in sessionStorage to indicate the modal has been shown
-            sessionStorage.setItem("modalShown", "true");
-        }
+      // Check if the modal has already been shown this session
+      if (event.clientY <= 0 && !sessionStorage.getItem("modalShown")) {
+        showModal();
+        // Set a flag in sessionStorage to indicate the modal has been shown
+        sessionStorage.setItem("modalShown", "true");
+      }
     }
-});
+  });
 
 
   // Close button functionality
   $(".btn-close-it").on("click", function () {
-      $(".confused-modal").fadeOut(500, function() {
-          $("body").removeClass("popup-active"); // Remove class from body
+    $(".confused-modal").fadeOut(500, function () {
+      $("body").removeClass("popup-active"); // Remove class from body
+    });
+  });
+
+
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Target the "Send Message" button
+  const sendButton = document.querySelector("#contactModal .btn-primary");
+  console.log("got here");
+  console.log(sendButton);
+  console.log(document.querySelector('input[placeholder="Enter your name"]').value);
+
+  sendButton.addEventListener("click", function (e) {
+    e.preventDefault(); // Prevent default button behavior
+    console.log("sendButton");
+    console.log(document.querySelector('input[placeholder="Enter your name"]').value);
+
+    // Replace with your Google Apps Script URL
+    const scriptURL = "https://script.google.com/macros/s/AKfycbw60KFSSntgGpH_sOECgeazDXo0UlVIX4YgLjQ-wXTwCC7nZQtQ8cMwY90H4K6QTccb5A/exec";
+
+    // Collect form data
+    const formData = {
+      firstName: document.querySelector('input[placeholder="Enter your name"]').value,
+      email: document.querySelector('input[placeholder="Enter your email"]').value,
+      phone: document.querySelector('input[placeholder="Enter your phone number"]').value,
+      msg: document.querySelector('textarea[placeholder="How can we help you? Brief description of care needs."]').value || "No message provided",
+    };
+    console.log(formData);
+
+    // Send data to Google Apps Script
+    fetch(scriptURL, {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Message sent successfully!");
+          document.getElementById("contactForm").reset(); // Reset the form
+        } else {
+          alert("Error sending message. Please try again.");
+        }
+      })
+      .catch((error) => {
+        alert("Error: " + error.message);
       });
   });
-    
-    
 });
+
+
