2eed08d4e6e68a89f9bb7e7e8025883501f65709
ronyderra <derrarony@gmail.com>
2025-01-14 16:17:58 +0200

    push

--- trk.js
+++ trk.js
@@ -1,55 +1,65 @@
+const webAppUrl = 'https://script.google.com/macros/s/AKfycby4CJO9qsw-Pk2Gr4RGQsSlvxcQRT4OCSgY27Id7YlJbSQt5weD-3wTZZ-5eVge2B91/exec'; // Replace with your Web App URL
+const now = new Date();
+const date = now.toLocaleDateString();
+const time = now.toLocaleTimeString();
+const urlParams = new URLSearchParams(window.location.search);
+const gclid = urlParams.get('gclid') || 'N/A';
+const domain = window.location.hostname;
+const fullRequest = window.location.href;
+const params = {};
+const subs = {};
+let index = 1;
+urlParams.forEach((value, key) => {
+    subs[`sub${index}`] = `${value}`;
+    index++;
+});
+const logData = {
+    date: date,
+    time: time,
+    gclid: gclid,
+    domain: domain,
+    fullRequest: fullRequest,
+    ...subs,
+    actionType: ""
+};
+
 function logRequestDetails() {
-    const now = new Date();
-    const date = now.toLocaleDateString();
-    const time = now.toLocaleTimeString();
-    const urlParams = new URLSearchParams(window.location.search);
-    const gclid = urlParams.get('gclid') || 'N/A'; // Get gclid if present
-    const domain = window.location.hostname;
-    const fullRequest = window.location.href;
-    const params = {};
-
-
-      // Map query parameters to sub1, sub2, sub3, etc.
-      const subs = {};
-      let index = 1;
-    // Extract all query parameters
-    urlParams.forEach((value, key) => {
-        subs[`sub${index}`] = `${value}`;
-        index++;
-    });
-
-    // Fetch IP Address
-    fetch('https://api64.ipify.org?format=json')
+    logData.actionType = "request"
+    fetch(webAppUrl, {
+        method: "POST",
+        redirect: "follow",
+        headers: new Headers({
+            "Content-Type": "text/plain;charset=utf-8",
+        }),
+        body: JSON.stringify(logData)
+    })
         .then(response => response.json())
         .then(data => {
-            const logData = {
-                date: date,
-                time: time,
-                gclid: gclid,
-                domain: domain,
-                fullRequest: fullRequest,
-                ...subs,
-                actionType: "request"
-            };
-
-            // Send data to Google Sheet
-            const webAppUrl = 'https://script.google.com/macros/s/AKfycby4CJO9qsw-Pk2Gr4RGQsSlvxcQRT4OCSgY27Id7YlJbSQt5weD-3wTZZ-5eVge2B91/exec'; // Replace with your Web App URL
-            fetch(webAppUrl, {
-                method: "POST",
-                redirect: "follow",
-                headers: new Headers({
-                  "Content-Type": "text/plain;charset=utf-8",
-                }),
-                body: JSON.stringify(logData)
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Data saved to Google Sheet:', data);
-            })
-            .catch(err => console.error('Error saving data:', err));
+            console.log('Data saved to Google Sheet:', data);
         })
-        .catch(err => console.error('Failed to fetch IP:', err));
+        .catch(err => console.error('Error saving data:', err));
+}
+logRequestDetails();
+
+function logLinkClick(event) {
+    logData.actionType = "lpClick"
+    console.log(event.target.hostname);
+    logData.offer = event.target.hostname || "Uknown"
+    fetch(webAppUrl, {
+        method: "POST",
+        redirect: "follow",
+        headers: new Headers({
+            "Content-Type": "text/plain;charset=utf-8",
+        }),
+        body: JSON.stringify(logData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Data saved to Google Sheet:', data);
+        })
+        .catch(err => console.error('Error saving data:', err));
 }
 
-// Call the function to log details
-logRequestDetails();
+document.querySelectorAll('a.trackable-link').forEach(link => {
+    link.addEventListener('click', logLinkClick);
+});
